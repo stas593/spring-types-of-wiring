d323582736a50d5f5848e04907017d9160900898
Станислав <yastas593@mail.ru>
2021-05-14 21:32:45 +0300

    ready

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,11 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    @Autowired
+    public Needle7 needle7;
 }
